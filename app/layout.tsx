8e07cb9758a305b73d82565e90a877ad54ea436a
Usman Abdullah <usmanabdullahbn@gmail.com>
2026-01-12 18:58:28 +0500

    Update Images

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`font-sans antialiased`}>
+      <body className={`font-sans antialiased`} suppressHydrationWarning={true}>
         {children}
         <Analytics />
       </body>
